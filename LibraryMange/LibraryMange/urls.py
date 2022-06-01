b657bf5ced39bc66beee51108de5b5a8fd437923
Ridowan-sajid <sajidridowan7@gmail.com>
2022-06-01 21:32:58 +0600

    add auth

--- LibraryMange/LibraryMange/urls.py
+++ LibraryMange/LibraryMange/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Book import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookList/',views.BookList.as_view(),name="bookList"),
     path('bookRud/<int:pk>/',views.BookListRUD.as_view(),name="bookRud"),
+    path('auth/',include('rest_framework.urls',
+                         namespace='rest_framework')),
 
     path('gettoken/',TokenObtainPairView.as_view(),name="token-obtain"),
     path('refreshtoken/',TokenRefreshView.as_view(),name="token-refresh"),
